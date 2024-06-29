1191a704ac2e61e45f08b464532ec920050c8347
hastrodev <matheusmodolodev@gmail.com>
2024-06-29 12:53:26 -0300

    Alterações

--- models/categoria_model.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-from typing import Optional
-from models.curso_model import Curso
-
-
-@dataclass
-class Categoria:
-    id: Optional[int] = None
-    nome: Optional[str] = None
--- util/html.py
+++
@@ -1,4 +0,0 @@
-def ler_html(nome_arquivo: str) -> str:
-    with open(f"html/{nome_arquivo}.html", "r", encoding="utf-8") as arquivo:
-        conteudo = arquivo.read()        
-        return conteudo
